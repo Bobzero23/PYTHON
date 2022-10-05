8d30397cc7d48d76ae12f9a6e3eca41e50b616ad
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-05 23:33:45 +0300

    COMMNETS AND VARIABLES

---
+++ syntax/Commnets.py
@@ -0,0 +1,2 @@
+#This is how you comment in Python
+print("use hashtag to comment")
---
+++ syntax/Variables.py
@@ -0,0 +1,8 @@
+#In Python varibles are created when you assign a value to it
+from cgi import print_arguments
+from re import A
+
+
+age = 5
+name = "Bobzero"
+print(age)
