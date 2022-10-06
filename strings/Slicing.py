006edd102577e5a46b69692119f3e4c9decf7da9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-06 11:07:41 +0300

    SLCIING

---
+++ strings/Slicing.py
@@ -0,0 +1,15 @@
+#Slicing the characters in String from 2 - 7 but 7 is not included
+a = "I don't know what you did, did to me.."
+print(a[2:7])
+
+#Slicing from the start
+b = "Hey miss lady in the yellow"
+print(b[:8])
+
+#Slicing to the end
+c = "I wanna be a billionare"
+print(c[8:])
+
+#Negative indexing
+d = "Don't you open up the window "
+print(d[-7:-1])
