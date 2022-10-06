82b201008bacfcaf11356eceea2f6b9d0213d235
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-06 08:15:31 +0300

    VARIABLE NAMES

---
+++ syntax/String_variable.py
@@ -0,0 +1,8 @@
+#String variables can be created using "double quotes" or 'single quotes'
+
+#creating string variables
+first_name = 'Bobzero'
+last_name = "TheProblem"
+
+print(first_name)
+print(last_name)
---
+++ syntax/Variable_names.py
@@ -0,0 +1,17 @@
+#There are several ways of creating variables
+
+#Camel case
+from cgi import print_arguments
+
+
+myFirstName = "Bobzero"
+
+#Snake case
+my_first_name = "Bobzero"
+
+#Pascal case
+MyFirstName = "Bobzero"
+
+print(myFirstName)
+print(my_first_name)
+print(MyFirstName)
