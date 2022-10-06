c8be7cbcf1180fc3d65f569a76dfafa94d7029a3
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-06 11:27:58 +0300

    MDOIFYING STRINGS

---
+++ strings/Modify.py
@@ -0,0 +1,21 @@
+#Demonstration on how to modify Strings
+
+#UpperCase
+a = "at the night show"
+print(a.upper())
+
+#LowerCase
+b = "ANYTHING CAN HAPPEN"
+print(b.lower())
+
+#Removing the whitespace at the start and end
+c = " Hospital "
+print(c.strip())
+
+#Replacing Strings
+d = "hahadahaha"
+print(d.replace("d","h"))
+
+#Splitting Strings
+e = "Split, here."
+print(e.split(","))
