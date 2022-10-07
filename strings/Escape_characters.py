6e652ab33def60569c06cc63eb2376f4498f8c15
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-07 11:37:04 +0300

    ESCAPE CHARACTERS

---
+++ strings/Escape_characters.py
@@ -0,0 +1,31 @@
+#Demonstration on how to use escape Characters
+
+#For double quotes \"
+from cmath import pi
+
+
+print("\"Hello\"")
+
+#For single quotes
+print("\'Hello\'")
+
+#For backslash
+print("\\Hello\\")
+
+#For new line
+print("Hello\nWorld")
+
+#For carriage return
+print("Hello\rWorld")
+
+#For tab
+print("Hello\tWorld")
+
+#For Backspace
+print("Hello\bWorld")
+
+#For Hex values
+print("\x48\x65\x6c\x6c\x6f")
+
+#For Octal values
+print("\110\145\154\154\157")
