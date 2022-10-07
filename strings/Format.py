8a7d2cd558fb724e6f317108b9f637421494dd9e
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-07 11:21:31 +0300

    FORMATTING

--- strings/Format.py
+++ strings/Format.py
@@ -5,3 +5,17 @@ b = "I am {} years old"
 
 #formatting Strings with Integer
 print(b.format(a))
+
+#Formatting multiple integers in String
+position = 1
+age = 35
+year = 1987
+sentence = "The {}'st winner is {} years old and was born in {}"
+print(sentence.format(position, age, year))
+
+#Indexing while formatting to make sure the arguments are in the right place
+one = 1
+two = 2
+three = 3
+counting = "let's say this is one({1}), this is two({2}) and this is three({0})"
+print(counting.format(three, one, two))
