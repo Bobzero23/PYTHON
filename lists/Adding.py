2a1158b2b9b7df6daa9afddb9b36e6665b8165e9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-10 10:48:40 +0300

    ADDING A LIST

---
+++ lists/Adding.py
@@ -0,0 +1,23 @@
+#Demonstration on how to add items in the list
+
+#Creating a list
+names = ["Bob", "Kombo", "Oma", "Mido", "Mamalao", "Mamba", "Kitufe"]
+
+#Displaying the list
+print(names)
+
+#Adding a name to the list
+names.append("Sheby")
+
+#Displaying the list
+print(names)
+
+#Creating lists
+country = []
+cities = ["Dar", "Dodoma", "Tanga", "Zanzibar"]
+
+#Adding a list to another list
+country.extend(cities)
+
+#Displaying the list
+print(country)
