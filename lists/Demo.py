e40673e5c0901fb8d31212d190da1e77a9f0764d
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-10 10:16:22 +0300

    LISTS DEMO

---
+++ lists/Demo.py
@@ -0,0 +1,24 @@
+#Demonstration on how to deal with lists
+
+
+#Lists are changeable, ordered and allow duplicates
+#Lists can contain multiple data types
+#You can also create a list using a constructor 
+#All lists are object and their type is <list>
+
+
+#Creating a fruits list
+fruits = ["Mango", "Orange", "Apple", "Strawberry", "Banana", "Avocado"]
+#Displaying the fruit list
+print(fruits)
+#Displaying the length of the fruit list
+print("The length of the fruits list is:", len(fruits))
+#Getting the type of the list
+print("The type of the fruits list is: ", type(fruits))
+
+#Creating a list using constructor
+thislist = list(("Bob", 23, "Zero", 0, False))
+#Displaying the list
+print("Constructor list is: ", thislist)
+
+
