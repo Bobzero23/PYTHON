e829de6c4a5ed066ede0c428b12a544357eaa8d3
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-10 13:25:38 +0300

    FINDING THE AVERAGE

---
+++ DATA MINING/Average.py
@@ -0,0 +1,20 @@
+#Demonstration on how to find the Average of the list 
+
+#Creating the list
+from itertools import count
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+#Finding the sum and the number of elements
+counter = 0
+sum = 0
+for i in numbers:
+    sum = sum + i
+    counter = counter + 1
+
+#Calculating the average
+average = sum / counter
+
+#Displaying the average
+print("The average of the list is: ", average)
