db444637e7df35b904c75a0629edb1b5daf7c16e
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-17 18:13:21 +0300

    FILM SAMPLE WITH DOT PRODUCT

---
+++ DATA MINING/modules/FilmExample.py
@@ -0,0 +1,14 @@
+#If you don't get what is going on check the class notes
+
+#imports
+import numpy as np
+
+#initializations
+A = np.array([5, 1, 3])
+M = np.array([4, 5, 1])
+
+#multiplying using dot product method
+mul = np.dot(A, M)
+
+#displaying the multiplication
+print(mul)
