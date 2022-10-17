88cb5e948744612b1d9d341a6e8e79c3fd39b043
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-17 18:09:28 +0300

    NUMPY INTRO

--- DATA MINING/modules/Dot_prouduct.py
+++ DATA MINING/modules/Dot_prouduct.py
@@ -1,7 +1,13 @@
+# A little example of how to create arrays using numpy
+
+#importing numpy
 import numpy as np
 
+#initializing an array
 x = np.array([3, 5, 7])
 
+#displaying the array
 print(x)
 
+#displaying the sum of the array
 print(sum(x))
