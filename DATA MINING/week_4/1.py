464d6f7994fe309e2c2c6646976ef735cfdedbf4
Bobzero <rajabu.mustafa.00@gmail.com>
2022-11-03 22:48:51 +0300

    WEEK 4 DATA MINING

--- DATA MINING/week_4/1.py
+++ DATA MINING/week_4/1.py
@@ -23,3 +23,19 @@ start_date = d2
 
 data = yfinance.download('TSLA', start= start_date, end= end_date, progress= False)
 data.head()
+
+#indexing the data according to date
+data["date"] = data.index
+
+#
+data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+
+#
+data.reset_index(drop= True, inplace= True)
+
+#drawing the graphic
+#x eksenindeki tarihi alacak data
+#sonra acilis verileri ve kapanis verilerini al
+graphics = go.Figure(data=[go.Candlestick(x= data["date"], open= data["Open"], close= data["close"])])
+
+
