9ae499329198893a1ca6425af771250df692ce69
Bobzero <rajabu.mustafa.00@gmail.com>
2022-11-04 11:12:51 +0300

    LITTLE MODIFICATION

--- DATA MINING/week5/Yt1.py
+++ DATA MINING/week5/Yt1.py
@@ -1,6 +1,49 @@
 import pandas as pd
-import matplotlib as plt
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
 
-#olm ne ugrastim be, boyle bisey olamaz allah kahretsin
-data = pd.read_csv('C:\\data.csv')
-print(data.head())
+dataset = pd.read_csv("User_Data.csv")
+
+x = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+sc_x = StandardScaler()
+xtrain = sc_x.fit_transform(xtrain)
+xtest = sc_x.transform(xtest)
+
+print (xtrain[0:10, :])
+
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(xtrain, ytrain)
+y_pred = classifier.predict(xtest)
+
+cm = confusion_matrix(ytest, y_pred)
+print ("Confusion Matrix : \n", cm)
+
+print ("Accuracy : ", accuracy_score(ytest, y_pred))
+
+X_set, y_set = xtest, ytest
+
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01), np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.75, cmap = ListedColormap(('red','green')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+  plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
