2beacfb4711b8778542808cac5c59a7f893cb426
Bobzero <rajabu.mustafa.00@gmail.com>
2022-11-04 13:47:09 +0300

    DISPLAYING THE DATA SET

--- DATA MINING/week5/youtube/1.py
+++ DATA MINING/week5/youtube/1.py
@@ -1,1 +1,9 @@
 #here we gonna see how to view the data set
+
+#imports
+import pandas as pd
+
+#reading the data set
+df = pd.read_csv('C:\\Users\\Sony\\Documents\\BOB\\data.csv')
+#displaying the data set
+print(df)
