71bdbfc317d8242e21e6ef7911aff009c9fc8a67
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-29 23:45:47 +0300

    string:- substring and contains methods implementations

---
+++ learnings/Strings.py
@@ -0,0 +1,12 @@
+x = "input"
+print(len(x))
+
+#substring
+print("substring 1 =", x[2:])
+print("substring 1 =", x[1:3])
+print("substring 1 =", x[-3:])
+print("substring 1 =", x[-3:-1])
+
+#contains
+print("contains", ("put" in x))
+
