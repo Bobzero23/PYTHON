d8095aeed6bb5468b8ecfc06e032d6aa278f390a
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-29 23:30:28 +0300

    variables complete

---
+++ Variables.py
@@ -0,0 +1,23 @@
+x = 4
+print("x =", x)
+
+x = 3 / 4
+print("x =", x)
+
+x = 3 // 4
+print("x =", x)
+
+x = 2 ** 3
+print("x =", x)
+
+x = 4 % 2
+print("x =", x)
+
+x = int(3.5)
+print("x =", x)
+
+x = max(3, 5)
+print("max is =", x)
+
+x = min(2, 5)
+print("min is =", x)
