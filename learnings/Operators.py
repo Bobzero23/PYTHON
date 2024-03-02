c8b59205f527aa8a96067dc42a369b47563ae757
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-02 13:05:31 +0300

    operators implementation complete

---
+++ learnings/Operators.py
@@ -0,0 +1,11 @@
+x = input()
+y = "house"
+
+# This checks the characters
+print(x == y)
+
+# This checks the objects
+print(x is y)
+
+# These operators are same as the operators in Java
+# <, >, >=, <= and there is also is 'and', 'or' and 'not'
