4d77978dc5d520b0545c9afa91a41ab4b506b578
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-03 11:16:09 +0300

    detecting object and putting their confidence value

--- object_detection/murtaza_tutorial/object_detection_with_yolo/on_webcam/main.py
+++ object_detection/murtaza_tutorial/object_detection_with_yolo/on_webcam/main.py
@@ -3,14 +3,17 @@ import cv2
 # cvzone displaying all the detection
 import cvzone
 
+# for rounding the confidence value
+import math
+
 # this is actually the id number of your camera
 # if you have multiple cameras connected you can use (1, 2, etc.)
 cap = cv2.VideoCapture(0)
 
 # setting the width of the cam
-cap.set(3, 680)
+cap.set(3, 1280)
 # setting the height of the cam
-cap.set(4, 720)
+cap.set(4, 1020)
 
 model = YOLO("../yolo_weights/yolov8n.pt")
 
@@ -24,9 +27,25 @@ while True:
         for box in boxes:
             # if you don't get the first index only it will not be able to unpack every box that is coming
             x1, y1, x2, y2 = box.xyxy[0]
+
             # converting the values to integer so that is more readable
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            print(x1, y1, x2, y2)
+
+            # calculating the width and the height
+            w, h = x2 - x1, y2 - y1
+
+            # now we create a rectangle for each object we can detect
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # to put some fancy box we can use binding box (bbx)
+            cvzone.cornerRect(img, (x1, y1, w, h))
+
+            # print(x1, y1, x2, y2) this will print the result of the normal method which is not the (cvzone)
+
+            # getting the confidence value (estimated value of the object)
+            conf = math.ceil(box.conf[0]*100) / 100
+            print(conf)
+            # putting the text of confidence zone to the frame
+            cvzone.putTextRect(img, f'{conf}', (x1, y1-20))
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
