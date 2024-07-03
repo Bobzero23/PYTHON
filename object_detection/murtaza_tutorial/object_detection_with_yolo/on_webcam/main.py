de028304733d3e9f07475c0f2fb849ae87e62b99
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-07-03 10:44:04 +0300

    object detection using YOLO with webcam - init commit

--- object_detection/murtaza_tutorial/object_detection_with_yolo/on_webcam/main.py
+++ object_detection/murtaza_tutorial/object_detection_with_yolo/on_webcam/main.py
@@ -12,10 +12,22 @@ cap.set(3, 680)
 # setting the height of the cam
 cap.set(4, 720)
 
-model = YOLO("")
+model = YOLO("../yolo_weights/yolov8n.pt")
 
 while True:
     success, img = cap.read()
+    results = model(img, stream=True)
+
+    # trying to get the value of the boxes
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # if you don't get the first index only it will not be able to unpack every box that is coming
+            x1, y1, x2, y2 = box.xyxy[0]
+            # converting the values to integer so that is more readable
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            print(x1, y1, x2, y2)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
